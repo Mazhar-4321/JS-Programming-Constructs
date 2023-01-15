8094b9457871682d95d64b1edcd2a1b106c9f3eb
Mazhar-4321 <syedmazharali742@gmail.com>
2023-01-15 20:27:03 +0530

    ADDED: Code To Flip A Coin

--- sequence_practice_problems/Main.js
+++ sequence_practice_problems/Main.js
@@ -5,8 +5,9 @@ const val4 = Math.floor(Math.random() * 10) + 100;
 const val5 = Math.floor(Math.random() * 10) + 100;
 console.log(Math.max(val5, Math.max(val4, Math.max(val3, Math.max(val1, val2)))))
 console.log(Math.min(val5, Math.min(val4, Math.min(val3, Math.min(val1, val2)))))
-console.log(5,7)
+console.log(5, 7)
 console.log(leapYear(2002))
+console.log(flipACoin())
 function checkDateBetweenMarch20AndJune20(d, m) {
     if (m < 3 || m > 6) {
         return false;
@@ -24,19 +25,22 @@ function checkDateBetweenMarch20AndJune20(d, m) {
         return true;
     return false;
 }
-function leapYear( year){
-    if(year<1582){
+function leapYear(year) {
+    if (year < 1582) {
         return false;
     }
-    else{
-        if(year%400==0){
+    else {
+        if (year % 400 == 0) {
             return true;
-        }else{
-            if(year%4==0&&year%100==0){
+        } else {
+            if (year % 4 == 0 && year % 100 == 0) {
                 return true;
-            }else{
+            } else {
                 return false;
             }
         }
     }
+}
+function flipACoin() {
+ return Math.floor(Math.random()*10)%2==0?"head":"tail"
 }
