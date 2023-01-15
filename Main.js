bc99d10c60416f99a11c80011ce13a04ad957939
Mazhar-4321 <syedmazharali742@gmail.com>
2023-01-15 20:11:26 +0530

    ADDED: Program To Find Min And Max Of 5 Random 3 Digit Numbers

---
+++ Main.js
@@ -0,0 +1,7 @@
+const val1=Math.floor(Math.random()*10)+100;
+const val2=Math.floor(Math.random()*10)+100;
+const val3=Math.floor(Math.random()*10)+100;
+const val4=Math.floor(Math.random()*10)+100;
+const val5=Math.floor(Math.random()*10)+100;
+console.log(Math.max(val5,Math.max(val4,Math.max(val3,Math.max(val1,val2)))))
+console.log(Math.min(val5,Math.min(val4,Math.min(val3,Math.min(val1,val2)))))
