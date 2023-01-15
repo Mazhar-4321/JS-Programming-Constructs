fb555bee6519d5c5a75ea6386313a9b120a23f0d
Mazhar-4321 <syedmazharali742@gmail.com>
2023-01-15 20:48:06 +0530

    ADDED: Code To Display Month Name

--- case/Main.js
+++ case/Main.js
@@ -15,3 +15,20 @@ function numberInWords( n){
     }
     console.log(s);
 }
+function monthNameOfANumber( n){
+    switch (n){
+        case 1 : return "January";
+        case 2 : return "Februaru";
+        case 3 : return "March";
+        case 4 : return "April";
+        case 5 : return "May";
+        case 6 : return "June";
+        case 7 : return "July";
+        case 8 : return "August";
+        case 9 : return "September";
+        case 10 : return "October";
+        case 11 : return "November";
+        case 12 : return "December";
+        default:return "Invalid Number";
+    }
+}
